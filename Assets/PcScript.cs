0f6e7e4e69b7dcc56ab31de90c656b1efe233f89
EnioAmarantes <enio.amarantes@gmail.com>
2023-05-22 20:32:59 -0300

    Adding shooting action

--- Assets/PcScript.cs
+++ Assets/PcScript.cs
@@ -5,16 +5,23 @@ public class PcScript : MonoBehaviour
     private Rigidbody rbd;
     private Quaternion startRotation;
     private float xMouse = 0;
+    private AudioSource somTiro;
 
+    public GameObject cabeca;
     public float vel;
     public float speedRotate;
+    public float dist = 100;
+    public LayerMask mascara;
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         vel = 10;
         speedRotate = 100;
         startRotation = transform.localRotation;
         rbd = GetComponent<Rigidbody>();
+        somTiro = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,6 +29,7 @@ public class PcScript : MonoBehaviour
     {
         Walk();
         Rotate();
+        Shoot();
     }
 
     private void Walk()
@@ -40,5 +48,27 @@ public class PcScript : MonoBehaviour
         Quaternion rotMouse = Quaternion.AngleAxis(xMouse, Vector3.up);
         transform.localRotation = startRotation * rotMouse;
         Debug.Log(rotMouse.ToString());
+    }
+
+    private void Shoot()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            //somTiro.Play;
+
+            if (Physics.Raycast(
+                cabeca.transform.position,
+                cabeca.transform.forward,
+                out hit,
+                dist,
+                mascara
+                ))
+            {
+                Rigidbody rbdAtingivel;
+                rbdAtingivel = hit.collider.GetComponent<Rigidbody>();
+                rbdAtingivel.AddForce(cabeca.transform.forward * 500);
+            }
+        }
     }
 }
